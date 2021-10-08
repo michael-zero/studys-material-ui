ea2d4b1f25520be63f4329f06fad9f57883e282a
Michael <mlmbufpi@gmail.com>
2021-10-08 10:50:01 -0300

    feat: add radiogroup and radio

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Typography, Button, Container, makeStyles, TextField} from '@material-ui/core'
+import {Typography, Button, Container, makeStyles, TextField, RadioGroup, Radio, FormControlLabel, FormControl, FormLabel} from '@material-ui/core'
 import {KeyboardArrowRight} from '@material-ui/icons';
 
 
@@ -20,6 +20,7 @@ export default function Create() {
   const [details, setDetails] = React.useState('')
   const [titleError, setTitleError] = React.useState(false)
   const [detailsError, setDetailsError] = React.useState(false)
+  const [category, setCategory] = React.useState('todos')
   
   const [error, setError] = React.useState(true)
 
@@ -64,10 +65,17 @@ export default function Create() {
     <form onSubmit={handleSubmit} noValidate autoComplete="off">
       <TextField error={titleError} onChange={(e) => setTitle(e.target.value)} fullWidth className={classes.field} required label='Note Title' variant="outlined" color="secondary"/>
       <TextField error={detailsError} onChange={(e) => setDetails(e.target.value)} multiline rows={4}  fullWidth className={classes.field} required label='Details' variant="outlined" color="secondary"/>
-      
-      <Typography>
-        {error ? 'true':'false'}
-      </Typography>
+    
+      <FormControl className={classes.field}>
+      <FormLabel>Note Category</FormLabel>
+      <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+        <FormControlLabel  value="money" control={<Radio/>} label="Money"/>
+        <FormControlLabel  value="todos" control={<Radio/>} label="Todos"/>
+        <FormControlLabel  value="reminder" control={<Radio/>} label="Reminder"/>
+        <FormControlLabel  value="work" control={<Radio/>} label="Work"/>
+      </RadioGroup>
+      </FormControl>
+
       
       <Button type="submit"
         disabled={error}
