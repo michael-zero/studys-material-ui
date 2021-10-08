2f593413c43a03ae535f16d06df43b7dec1b0deb
Michael <mlmbufpi@gmail.com>
2021-10-08 09:13:14 -0300

    feat: add icons and fonts

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,16 +1,38 @@
 import React from 'react'
-import {Typography, Button, Container} from '@material-ui/core'
+import {Typography, Button, Container, makeStyles} from '@material-ui/core'
+import {KeyboardArrowRight} from '@material-ui/icons';
+
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: '60px',
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: '20px'
+  }
+})
 
 export default function Create() {
+  
+  const classes = useStyles()
+
   return (
     <Container>
-     <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+     <Typography className={classes.title} variant="h6" component="h2" color="textSecondary" gutterBottom>
        Create a New Note
      </Typography>
 
      <Button type="submit"
+     className={classes.btn}
      color="secondary"
-     variant="contained">
+     variant="contained"
+     endIcon={<KeyboardArrowRight/>}
+     >
        Submit
      </Button>
     </Container>
