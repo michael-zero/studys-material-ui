6b6b603090461beb00745d04d18500625c25501a
Michael <mlmbufpi@gmail.com>
2021-10-08 12:04:03 -0300

    feat: create drawer navigator

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,43 @@
 import { makeStyles } from '@material-ui/styles'
 import React from 'react'
+import { Drawer, Typography } from '@material-ui/core'
 
+const drawerWidth = 240; 
 const useStyles = makeStyles({
+    root: {
+        display: 'flex'
+    },
     page: {
         backgroundColor: '#f9f9f9',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
     }
 })
 const Layout = ({children}) => {
     const classes = useStyles()
     return (
-        <div>
+        <div className={classes.root}>
             {/* app bar */}
 
             {/* side drawer */}
+            <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            anchor="left"
+            classes={{paper: classes.drawerPaper}}
+            >
+                <div>
+                    <Typography variant='h5'>
+                        robot notes
+                    </Typography>
+                </div>
+            </Drawer>
+
             <div className={classes.page}>
               {children}
             </div>
